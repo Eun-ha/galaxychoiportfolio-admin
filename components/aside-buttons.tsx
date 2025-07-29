5bdc14368aea477b29dfd1ece0106ffad3ecf513
CHOI EUNHA <galaxyceh@gmail.com>
2025-07-29 18:04:14 +0900

    adise-button 컴포넌트 코드 보완

--- components/aside-buttons.tsx
+++ components/aside-buttons.tsx
@@ -15,7 +15,7 @@ export async function AsideButtons(props: Props) {
         categories={[
           ...(path === "/admin"
             ? [
-                { button: "admin", slug: "/" },
+                { button: "admin", slug: "" },
                 { button: "main", slug: "main" },
                 { button: "skill", slug: "skill" },
                 { button: "work", slug: "work" },
